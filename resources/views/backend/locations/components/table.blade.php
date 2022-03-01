534cb1d957014fd3106d43cc3a008bd8cd92fa32
mydisain <info@mydisain.com>
2022-03-01 19:20:56 +0200

    Add files via upload

--- resources/views/backend/locations/components/table.blade.php
+++ resources/views/backend/locations/components/table.blade.php
@@ -4,16 +4,16 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header border-bottom">
-                    <h4 class="card-title">Locations</h4>
-                    <Button class="btn btn-primary waves-effect waves-float waves-light" data-toggle="modal" data-target="#location_modal" onclick="addNewLocation()">Add New Location</Button>
+                    <h4 class="card-title">Asukohad</h4>
+                    <Button class="btn btn-primary waves-effect waves-float waves-light" data-toggle="modal" data-target="#location_modal" onclick="addNewLocation()">Lisa asukoht</Button>
                 </div>
                 <div class="card-datatable col-12">
                     <table class="table datatables-ajax" id="location_table">
                         <thead>
                             <tr>
                                 <th></th>
-                                <th width="80%">Name</th>
-                                <th>Action</th>
+                                <th width="80%">Nimi</th>
+                                <th>Tegevused</th>
                             </tr>
                         </thead>
                         <tbody></tbody>
