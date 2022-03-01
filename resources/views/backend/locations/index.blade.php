2c3a80c9049a8eb73274aaaf15b8a2aa82a58afb
mydisain <info@mydisain.com>
2022-03-01 19:20:01 +0200

    Add files via upload

--- resources/views/backend/locations/index.blade.php
+++ resources/views/backend/locations/index.blade.php
@@ -14,12 +14,12 @@
         <div class="content-header-left col-md-9 col-12 mb-2">
             <div class="row breadcrumbs-top">
                 <div class="col-12">
-                    <h2 class="content-header-title float-left mb-0">Locations</h2>
+                    <h2 class="content-header-title float-left mb-0">Asukohad</h2>
                     <div class="breadcrumb-wrapper">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="index.html">Home</a>
                             </li>
-                            <li class="breadcrumb-item active">locations
+                            <li class="breadcrumb-item active">
                             </li>
                         </ol>
                     </div>
