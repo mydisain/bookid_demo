791ef7c48b57b2efe7ff4ad86b37605bdbac1c43
mydisain <info@mydisain.com>
2022-03-01 19:24:41 +0200

    Add files via upload

--- resources/views/auth/backend/signin.blade.php
+++ resources/views/auth/backend/signin.blade.php
@@ -3,11 +3,11 @@
 <!-- Login-->
 <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
     <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-        <h2 class="card-title font-weight-bold mb-1">Sign In Admin! </h2>
+        <h2 class="card-title font-weight-bold mb-1">Sisene BOOKid haldusesse! </h2>
         @if ($errors->has('fail'))
             <div class="alert alert-danger">
                 <div class="alert-body">
-                    Credentials are not valid.
+                    Kasutajat ei leitud.
                 </div>
             </div>
         @endif
@@ -19,7 +19,7 @@
             </div>
             <div class="form-group">
                 <div class="d-flex justify-content-between">
-                    <label for="login-password">Password</label><a href="page-auth-forgot-password-v2.html"><small>Forgot Password?</small></a>
+                    <label for="login-password">Parool</label><a href="page-auth-forgot-password-v2.html"><small>Unustasid parooli?</small></a>
                 </div>
                 <div class="input-group input-group-merge form-password-toggle">
                     <input class="form-control form-control-merge" id="login-password" type="password" name="login-password" placeholder="············" aria-describedby="login-password" tabindex="2" />
@@ -29,12 +29,12 @@
             <div class="form-group">
                 <div class="custom-control custom-checkbox">
                     <input class="custom-control-input" id="remember-me" name="login-remember" type="checkbox" tabindex="3" />
-                    <label class="custom-control-label" for="remember-me"> Remember Me</label>
+                    <label class="custom-control-label" for="remember-me"> Pea meeles</label>
                 </div>
             </div>
-            <button class="btn btn-primary btn-block" tabindex="4">Sign in</button>
+            <button class="btn btn-primary btn-block" tabindex="4">Logi sisse</button>
         </form>
-        <p class="text-center mt-2"><span>New on our platform?</span><a href="/signup"><span>&nbsp;Create an account</span></a></p>
+        <p class="text-center mt-2"><span>Sul ei ole veel kontot?</span><a href="/signup"><span>&nbsp;Loo konto</span></a></p>
     </div>
 </div>
 <!-- /Login-->
